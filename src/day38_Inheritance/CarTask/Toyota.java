e53f820447b4f4eb637f3697458ba3bc24a0161e
Layla <layla.biologie@gmail.com>
2022-01-13 10:56:00 -0500

    toyota class

---
+++ src/day38_Inheritance/CarTask/Toyota.java
@@ -0,0 +1,18 @@
+package day38_Inheritance.CarTask;
+
+public class Toyota extends Car {
+    public Toyota( String model, String color, int year, double price, double miles) {
+        super("Toyota", model, color, year, price, miles);
+    }
+
+public void reliable(){
+    System.out.println(brand+ " "+ model+ "is reliable");
+}
+
+
+
+
+
+
+
+}
